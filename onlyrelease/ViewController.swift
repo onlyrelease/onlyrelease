41c4dcd6b59da2b050cdc3ecb0ce60a8fc81880b
jinchengli <lc13353607581@163.com>
2024-11-29 09:22:35 +0800

    Initial Commit

---
+++ onlyrelease/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  onlyrelease
+//
+//  Created by 李金成 on 2024/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ onlyreleaseTests/onlyreleaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  onlyreleaseTests.swift
+//  onlyreleaseTests
+//
+//  Created by 李金成 on 2024/11/29.
+//
+
+import Testing
+@testable import onlyrelease
+
+struct onlyreleaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
